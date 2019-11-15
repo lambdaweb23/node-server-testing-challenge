f360677e5bdbd31586098dba7afa2e6650b65e2f
Kenna Lawrie <kenna@kennas-mbp-2.home>
2019-11-14 17:30:12 -0800

    migrations

--- data/migrations/20191114171359_favorites.js
+++ data/migrations/20191114171359_favorites.js
@@ -1,8 +1,12 @@
-
 exports.up = function(knex) {
+    return knex.schema.createTable("favorites", table =>{
+        table.increments();
+        table.string("fav")
+          .notNullable()
+          .unique();
+    })
+  };
   
-};
-
-exports.down = function(knex) {
-  
-};
+  exports.down = function(knex) {
+    return knex.schema.dropTableIfExists("favorites");
+  };
